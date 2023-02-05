755c4cf831c4d94589f0315219d64b91d2a74a10
niteshKrr <nkumar35101@gmail.com>
2023-02-05 14:26:02 +0530

    balance fetched

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import AllNFTaccrossChain from "@/components/AllNFTaccrossChain";
 import All_ERC20_token_ownby_address from "@/components/All_ERC20_token_ownby_address";
+import Balance_of_address from "@/components/Balance_of_address";
 import React from "react";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <div> <AllNFTaccrossChain/> </div>
       <div> <All_ERC20_token_ownby_address/> </div>
+      <div> <Balance_of_address/> </div>
     </>
   );
 }
